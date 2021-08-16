8d578e672e4411ed1de54a68193318010dea8891
Beibarys Kokanbekov <w59858@student.wsiz.edu.pl>
2021-08-16 17:09:08 +0600

    Add project files.

---
+++ HalykBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HalykBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Здравствуйте! Как вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ваш возраст?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 6)
+                Console.WriteLine("Здравствуйте " + name + "! Вам пора в садик");
+            else if ((age >= 6) && (age < 18))
+                Console.WriteLine("Здравствуйте " + name + "! Вам пора в в школу");
+            else
+                Console.WriteLine("Здравствуйте " + name + "! Вам пора в университет");
+        }
+    }
+}
